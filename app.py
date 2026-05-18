6bc80e5dd0f14031be8e2470041328665ad13a78
BikrantBasnet <bikrantbasnet25@gmail.com>
2026-05-18 14:36:16 +0545

    fixed rendering problems of shipment history and creation

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('login.html')
-
-@app.route('/login2')
-def login2():
-    return render_template('login.html')
+@app.route('/create-shipment')
+def create_shipment():
+    return render_template('create-shipment.html')
+@app.route('/shipment-history')
+def shipment_history():
+    return render_template('shipment-history.html')
 
 
 if __name__ == '__main__':
